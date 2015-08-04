7ff3889bb6dcf44ac2f9129ada38533895c825c7
mmelodea <mmelodea@cern.ch>
2015-08-04 17:05:24 -0300

    Improving...

--- NewProject/Developing/FME_Module/FastME.cxx
+++ NewProject/Developing/FME_Module/FastME.cxx
@@ -1,7 +1,11 @@
-///FastME Constructor
-///Author: Miquéias M. de Almeida 
+///:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+///::::::::::::::::::::::::::::::::::	FAST MATRIX ELEMENT CLASS MEMBERS DEFINITIONS	::::::::::::::::::::::::::::::::::
+///::::::::::::::::::::::::::::::::::          Author: Miquéias M. de Almeida 		:::::::::::::::::::::::::::::::
+///::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-#include "../PhsDrComputers/PHS_DRComputers.cxx"
+#include "../PhsDrComputers/FS4l_DrComputers.cxx"
+#include "../PhsDrComputers/FS4l2j_DrComputers.cxx"
+#include "../PhsDrComputers/FSlv2j_DrComputers.cxx"
 #include "FastME.h"
 #include <iostream>
 #include <ctime>
@@ -18,11 +22,7 @@
 using namespace std;
 
 
-///:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-///::::::::::::::::::::::::::::::::::	FAST MATRIX ELEMENT CLASS MEMBERS DEFINITIONS	::::::::::::::::::::::::::::::::::
-///::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-int FME::launchFME(TString Final_State, TString Model, TString Out_Name, TString Data_Path, TString MC_Sig_Path, TString MC_Bkg_Path, TString Tree_Name, TString Branch_Name){
+int FME::launchFME(TString Final_State, TString Model, TString Out_Name, TString Data_Path, TString MC_Sig_Path, TString MC_Bkg_Path, TString Tree_Name, TString Branch_Name, TString Resolution){
   const int nFS = 3;
   int FS = -1;
   TString FState[nFS] = {"4l","4l2j","lv2j"};
@@ -47,28 +47,27 @@ int FME::launchFME(TString Final_State, TString Model, TString Out_Name, TString
     
   switch(FS){
     case 0:
-      return FS_4l(Model,Out_Name,Data_Tree,MC_Sig_Tree,MC_Bkg_Tree,Branch_Name);
+      return FS_4l(Model,Out_Name,Data_Tree,MC_Sig_Tree,MC_Bkg_Tree,Branch_Name,Resolution);
       break;
-/*    
+      
     case 1:
-      return FS_4l2j(Model,Out_Name,Data_Tree,MC_Sig_Tree,MC_Bkg_Tree,Branch_Name);
+      return FS_4l2j(Model,Out_Name,Data_Tree,MC_Sig_Tree,MC_Bkg_Tree,Branch_Name,Resolution);
       break;
       
     case 2:
-      return FS_lv2j_DR(Model,TString Out_Name, TTree *Data_Tree, TTree *MC_Sig_Tree, TTree *MC_Bkg_Tree, TString Branch_Name);
+      return FS_lv2j(Model,Out_Name,Data_Tree,MC_Sig_Tree,MC_Bkg_Tree,Branch_Name,Resolution);
       break;
     
     default:
-      return FS_4l(Model,TString Out_Name, TTree *Data_Tree, TTree *MC_Sig_Tree, TTree *MC_Bkg_Tree, TString Branch_Name);
+      return FS_4l(Model,Out_Name,Data_Tree,MC_Sig_Tree,MC_Bkg_Tree,Branch_Name,Resolution);
       break;
-      */
   }
   return 0;
 }
 
 
 ///:::::::::::::::::::::	FULL LEPTONIC CASE	   :::::::::::::::::::::
-int FME::FS_4l(TString Model, TString Out_Name, TTree* Data_Tree, TTree* MC_Sig_Tree, TTree* MC_Bkg_Tree, TString Branch_Name){
+int FME::FS_4l(TString Model, TString Out_Name, TTree* Data_Tree, TTree* MC_Sig_Tree, TTree* MC_Bkg_Tree, TString Branch_Name, TString Resolution){
   ///For timming the process
   time_t start, stop;
   double seconds, elapsed_time;
@@ -124,10 +123,10 @@ int FME::FS_4l(TString Model, TString Out_Name, TTree* Data_Tree, TTree* MC_Sig_
     for(int s=0; s<nsig; s++){
       MC_Sig_Tree->GetEntry(s);
       
-            if(Model == "DR_Order_Res")   dr_test = FS4l_Res_DrOrder(Data,MC);
-       else if(Model == "DR_Order_noRes") dr_test = FS4l_noRes_DrOrder(Data,MC);
-       else if(Model == "PT_Order_Res")   dr_test = FS4l_Res_PtOrder(Data,MC);
-       else if(Model == "PT_Order_noRes") dr_test = FS4l_noRes_PtOrder(Data,MC);
+            if(Model == "DR_Order_Res")   dr_test = FS4l_Res_DrOrder(Data,MC,Resolution);
+       else if(Model == "DR_Order_noRes") dr_test = FS4l_noRes_DrOrder(Data,MC,Resolution);
+       else if(Model == "PT_Order_Res")   dr_test = FS4l_Res_PtOrder(Data,MC,Resolution);
+       else if(Model == "PT_Order_noRes") dr_test = FS4l_noRes_PtOrder(Data,MC,Resolution);
        else{
 	 cout<<"Model '"<<Model<<"' is not defined!"<<endl;
 	 throw exception();
@@ -138,10 +137,10 @@ int FME::FS_4l(TString Model, TString Out_Name, TTree* Data_Tree, TTree* MC_Sig_
     for(int b=0; b<nbkg; b++){
       MC_Bkg_Tree->GetEntry(b);
       
-            if(Model == "DR_Order_Res")   dr_test = FS4l_Res_DrOrder(Data,MC);
-       else if(Model == "DR_Order_noRes") dr_test = FS4l_noRes_DrOrder(Data,MC);
-       else if(Model == "PT_Order_Res")   dr_test = FS4l_Res_PtOrder(Data,MC);
-       else if(Model == "PT_Order_noRes") dr_test = FS4l_noRes_PtOrder(Data,MC);
+            if(Model == "DR_Order_Res")   dr_test = FS4l_Res_DrOrder(Data,MC,Resolution);
+       else if(Model == "DR_Order_noRes") dr_test = FS4l_noRes_DrOrder(Data,MC,Resolution);
+       else if(Model == "PT_Order_Res")   dr_test = FS4l_Res_PtOrder(Data,MC,Resolution);
+       else if(Model == "PT_Order_noRes") dr_test = FS4l_noRes_PtOrder(Data,MC,Resolution);
        else{
 	 cout<<"Model '"<<Model<<"' is not defined!"<<endl;
 	 throw exception();
@@ -184,7 +183,7 @@ int FME::FS_4l(TString Model, TString Out_Name, TTree* Data_Tree, TTree* MC_Sig_
   FME_Results->Close();
   
   time(&stop);
-  cout<<"\n::::::::::: Process Finished :::::::::::"<<endl;
+  cout<<"::::::::::: Process Finished :::::::::::"<<endl;
   cout<<":: SigFrac =      "<<sig_frac<<"%"<<endl;
   cout<<":: BkgFrac =      "<<bkg_frac<<"%"<<endl;
 
@@ -207,10 +206,10 @@ int FME::FS_4l(TString Model, TString Out_Name, TTree* Data_Tree, TTree* MC_Sig_
 }
 
 
-/*
+
 
 ///:::::::::::::::::::::	FULL LEPTONIC + 2JETS	    :::::::::::::::::::::
-int FME::FS_4l(TString Out_Name, TTree* Data_Tree, TTree* MC_Sig_Tree, TTree* MC_Bkg_Tree, TString Branch_Name){
+int FME::FS_4l2j(TString Model, TString Out_Name, TTree* Data_Tree, TTree* MC_Sig_Tree, TTree* MC_Bkg_Tree, TString Branch_Name, TString Resolution){
   ///For timming the process
   time_t start, stop;
   double seconds, elapsed_time;
@@ -230,7 +229,7 @@ int FME::FS_4l(TString Out_Name, TTree* Data_Tree, TTree* MC_Sig_Tree, TTree* MC
   MC_Bkg_Tree->SetBranchAddress(Branch_Name,&MC);
   int nbkg = MC_Bkg_Tree->GetEntries();
   
-  double minDR_toSig, minDR_toBkg, prob_sig_bkg, sig_frac, event_weight;
+  double dr_test, minDR_toSig, minDR_toBkg, prob_sig_bkg, sig_frac, event_weight;
   double bkg_frac, min_dr_sig, min_dr_bkg, sig_event_weight, bkg_event_weight;
   int signal = 0, background = 0, sig_neighbors, bkg_neighbors;
   TTree *FME_out = new TTree(Out_Name,"Fast Matrix Element Results");
@@ -264,13 +263,30 @@ int FME::FS_4l(TString Out_Name, TTree* Data_Tree, TTree* MC_Sig_Tree, TTree* MC
     
     for(int s=0; s<nsig; s++){
       MC_Sig_Tree->GetEntry(s);
-      double dr_test = std_DR(6,Data,MC);
+      
+            if(Model == "DR_Order_Res")   dr_test = FS4l2j_Res_DrOrder(Data,MC,Resolution);
+       else if(Model == "DR_Order_noRes") dr_test = FS4l2j_noRes_DrOrder(Data,MC,Resolution);
+       else if(Model == "PT_Order_Res")   dr_test = FS4l2j_Res_PtOrder(Data,MC,Resolution);
+       else if(Model == "PT_Order_noRes") dr_test = FS4l2j_noRes_PtOrder(Data,MC,Resolution);
+       else{
+	 cout<<"Model '"<<Model<<"' is not defined!"<<endl;
+	 throw exception();
+      }
       if(dr_test < min_dr_sig) min_dr_sig = dr_test;
       if(dr_test < phs_radius) sig_neighbors += 1;
     }
     for(int b=0; b<nbkg; b++){
       MC_Bkg_Tree->GetEntry(b);
-      double dr_test = std_DR(6,Data,MC);
+      
+            if(Model == "DR_Order_Res")   dr_test = FS4l2j_Res_DrOrder(Data,MC,Resolution);
+       else if(Model == "DR_Order_noRes") dr_test = FS4l2j_noRes_DrOrder(Data,MC,Resolution);
+       else if(Model == "PT_Order_Res")   dr_test = FS4l2j_Res_PtOrder(Data,MC,Resolution);
+       else if(Model == "PT_Order_noRes") dr_test = FS4l2j_noRes_PtOrder(Data,MC,Resolution);
+       else{
+	 cout<<"Model '"<<Model<<"' is not defined!"<<endl;
+	 throw exception();
+      }
+      
       if(dr_test < min_dr_bkg) min_dr_bkg = dr_test;
       if(dr_test < phs_radius) bkg_neighbors += 1;
     }    
@@ -307,7 +323,7 @@ int FME::FS_4l(TString Out_Name, TTree* Data_Tree, TTree* MC_Sig_Tree, TTree* MC
   FME_Results->Close();
   
   time(&stop);
-  cout<<"\n::::::::::::::::::::: Process Finished :::::::::::::::::::::"<<endl;
+  cout<<"::::::::::::::::::::: Process Finished :::::::::::::::::::::"<<endl;
   cout<<"SigFrac = "<<sig_frac<<"%"<<endl;
   cout<<"BkgFrac = "<<bkg_frac<<"%"<<endl;
 
@@ -333,7 +349,7 @@ int FME::FS_4l(TString Out_Name, TTree* Data_Tree, TTree* MC_Sig_Tree, TTree* MC
 
 
 ///:::::::::::::::::::::	SEMI-LEPTONIC CASE	   :::::::::::::::::::::
-int FME::FS_lv2j(){
+int FME::FS_lv2j(TString Model, TString Out_Name, TTree* Data_Tree, TTree* MC_Sig_Tree, TTree* MC_Bkg_Tree, TString Branch_Name, TString Resolution){
   ///For timming the process
   time_t start, stop;
   double seconds, elapsed_time;
@@ -344,51 +360,124 @@ int FME::FS_lv2j(){
   cout<<"\n:::::::::::::::: Starting FastME Processing ::::::::::::::::"<<endl;
   cout<<"Model Chosen:  lv2j"<<endl;
   cout<<"------------------------------------------------------------"<<endl;
+  
+  Float_t Data[4][3][2], MC[4][3][2];
+  Data_Tree->SetBranchAddress(Branch_Name,&Data);
+  int ndata = Data_Tree->GetEntries();
+  MC_Sig_Tree->SetBranchAddress(Branch_Name,&MC);
+  int nsig = MC_Sig_Tree->GetEntries();
+  MC_Bkg_Tree->SetBranchAddress(Branch_Name,&MC);
+  int nbkg = MC_Bkg_Tree->GetEntries();
+  
+  double dr_test, minDR_toSig, minDR_toBkg, prob_sig_bkg, sig_frac, event_weight;
+  double bkg_frac, min_dr_sig, min_dr_bkg, sig_event_weight, bkg_event_weight;
+  int signal = 0, background = 0, sig_neighbors, bkg_neighbors;
+  TTree *FME_out = new TTree(Out_Name,"Fast Matrix Element Results");
+  FME_out->SetDirectory(0);
+  FME_out->Branch("minDR_toSig",&minDR_toSig);
+  FME_out->Branch("minDR_toBkg",&minDR_toBkg);
+  FME_out->Branch("SigFrac",&sig_frac);
+  FME_out->Branch("BkgFrac",&bkg_frac);
+  FME_out->Branch("P_SB",&prob_sig_bkg);
+  FME_out->Branch("WSig_ToEvent",&sig_event_weight);
+  FME_out->Branch("WBkg_ToEvent",&bkg_event_weight);
+  FME_out->Branch("Event_Weight",&event_weight);
 
   
-  double dpt = 0, deta = 0, dphi = 0;
-  double sum_dpt2 = 0, sum_deta2 = 0, sum_dphi2 = 0, event_distance = -1;
-  int start=0;
-
-    ///Electron
-    dpt  = (Data[start][0][0] - MC[start][0][0])/Data[start][0][1];
-    deta = (Data[start][1][0] - MC[start][1][0])/Data[start][1][1];
-    dphi = (Data[start][2][0] - MC[start][2][0])/Data[start][2][1];
-    sum_dpt2  += pow(dpt,2);
-    sum_deta2 += pow(deta,2);
-    sum_dphi2 += pow(dphi,2);
+  for(int i=0; i<ndata; i++){
+    if(i % (ndata/10) == 0) cout<<"Remaining Data: "<<ndata-i<<endl;
+    Data_Tree->GetEntry(i);
     
-    ///MET
-    start = 1;
-    dpt  = (Data[start][0][0] - MC[start][0][0])/Data[start][0][1];
-    deta = (Data[start][1][0] - MC[start][1][0])/Data[start][1][1];
-    dphi = (Data[start][2][0] - MC[start][2][0])/Data[start][2][1];
-    sum_dpt2  += pow(dpt,2);
-    sum_deta2 += pow(deta,2);
-    sum_dphi2 += pow(dphi,2);
+    //Reseting Variables
+    minDR_toSig   	= pedestal;
+    minDR_toBkg   	= pedestal;
+    prob_sig_bkg  	= pedestal;
+    sig_frac      	= pedestal;
+    bkg_frac	  	= pedestal;
+    sig_event_weight 	= pedestal;
+    bkg_event_weight 	= pedestal;
+    min_dr_sig    	= 1.E15;
+    min_dr_bkg    	= 1.E15;
+    sig_neighbors 	= 0;
+    bkg_neighbors 	= 0;
+    
+    for(int s=0; s<nsig; s++){
+      MC_Sig_Tree->GetEntry(s);
+      
+            if(Model == "Jet_DR_Order") dr_test = FSlv2j_Jet_DrOrder(Data,MC,Resolution);
+       else if(Model == "Jet_PT_Order") dr_test = FSlv2j_Jet_PtOrder(Data,MC,Resolution);
+       else{
+	 cout<<"Model '"<<Model<<"' is not defined!"<<endl;
+	 throw exception();
+      }
+      if(dr_test < min_dr_sig) min_dr_sig = dr_test;
+      if(dr_test < phs_radius) sig_neighbors += 1;
+    }
+    for(int b=0; b<nbkg; b++){
+      MC_Bkg_Tree->GetEntry(b);
+      
+            if(Model == "Jet_DR_Order") dr_test = FSlv2j_Jet_DrOrder(Data,MC,Resolution);
+       else if(Model == "Jet_PT_Order") dr_test = FSlv2j_Jet_PtOrder(Data,MC,Resolution);
+       else{
+	 cout<<"Model '"<<Model<<"' is not defined!"<<endl;
+	 throw exception();
+      }
+      
+      if(dr_test < min_dr_bkg) min_dr_bkg = dr_test;
+      if(dr_test < phs_radius) bkg_neighbors += 1;
+    }    
+    
+    ///Getting the discriminant value
+    if(min_dr_sig == 0 && min_dr_bkg == 0){ 
+      min_dr_sig = 1;
+      min_dr_bkg = 1;
+    }
+      prob_sig_bkg = min_dr_sig/(min_dr_sig + min_dr_bkg);
+      minDR_toSig = min_dr_sig;
+      minDR_toBkg = min_dr_bkg;
+      
+    ///Checking amount of kind choices
+    if(prob_sig_bkg < cut) signal += 1;
+    if(prob_sig_bkg > cut) background += 1;
+    if(i == ndata-1){
+      sig_frac = (signal/float(ndata))*100;
+      bkg_frac = (background/float(ndata))*100;
+    }
+    
+    ///Getting Event Weights
+    sig_event_weight = sig_neighbors/float(nsig);
+    bkg_event_weight = bkg_neighbors/float(nbkg);
+    event_weight     = sig_event_weight/(sig_event_weight + bkg_event_weight);
+    
+      
+      ///Saving Current Results
+      FME_out->Fill();
+  }
   
-    ///Jet 1
-    start = 2;
-    dpt  = (Data[start][0][0] - MC[start][0][0])/Data[start][0][1];
-    deta = (Data[start][1][0] - MC[start][1][0])/Data[start][1][1];
-    dphi = (Data[start][2][0] - MC[start][2][0])/Data[start][2][1];
-    sum_dpt2  += pow(dpt,2);
-    sum_deta2 += pow(deta,2);
-    sum_dphi2 += pow(dphi,2);
+  TFile *FME_Results = new TFile(Out_Name+"_Results.root","recreate");
+  FME_out->Write();
+  FME_Results->Close();
   
-    ///Jet 2
-    start = 3;
-    dpt  = (Data[start][0][0] - MC[start][0][0])/Data[start][0][1];
-    deta = (Data[start][1][0] - MC[start][1][0])/Data[start][1][1];
-    dphi = (Data[start][2][0] - MC[start][2][0])/Data[start][2][1];
-    sum_dpt2  += pow(dpt,2);
-    sum_deta2 += pow(deta,2);
-    sum_dphi2 += pow(dphi,2);
+  time(&stop);
+  cout<<"::::::::::::::::::::: Process Finished :::::::::::::::::::::"<<endl;
+  cout<<"SigFrac = "<<sig_frac<<"%"<<endl;
+  cout<<"BkgFrac = "<<bkg_frac<<"%"<<endl;
+
+      
+  seconds = difftime(stop,start);
+  if(seconds < 60) elapsed_time = seconds;
+  if(seconds >= 60){
+    elapsed_time = seconds/60.;
+    unity = "min.";
+  }
+  if(seconds >= 3600){
+    elapsed_time = seconds/3600.;
+    unity = "h";
+  }
+  cout<<" Time Consumed: "<<elapsed_time<<" "<<unity<<endl;
+  cout<<"------------------------------------------------------------"<<endl;
   
-  event_distance = sqrt(sum_dpt2 + sum_deta2 + sum_dphi2);
-  return event_distance;
   
   return 0; //if process well finished
 }
-*/
 ///====================================================================================================
