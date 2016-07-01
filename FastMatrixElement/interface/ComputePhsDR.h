a3a64e4dd4b1ca9e95e1f21281415d688796f58f
MiqueiasMelodeAlmeida <mmelodea@cern.ch>
2016-07-01 15:45:57 +0200

    Fixing no flavor constraint option

--- FastMatrixElement/interface/ComputePhsDR.h
+++ FastMatrixElement/interface/ComputePhsDR.h
@@ -172,10 +172,6 @@ TTree *ComputePhsDR(FmeSetup Setup){
 	    else if(FlavorConstraint == "false"){
               if( (abs(DataId[idt])!= 11 && abs(DataId[idt])!= 13) && (abs(McId[imc])== 11 || abs(McId[imc])== 13) ) continue;
               if( (abs(DataId[idt])== 11 || abs(DataId[idt])== 13) && (abs(McId[imc])!= 11 && abs(McId[imc])!= 13) ) continue;
-	      //if(abs(DataId[idt])== 11 && (abs(McId[imc])!= 11 || abs(McId[imc])!= 13)) continue;
-	      //if(abs(DataId[idt])== 13 && (abs(McId[imc])!= 11 || abs(McId[imc])!= 13)) continue;
-	      //if(abs(McId[imc])== 11 && (abs(DataId[idt])!= 11 || abs(DataId[idt])!= 13)) continue;
-	      //if(abs(McId[imc])== 13 && (abs(DataId[idt])!= 11 || abs(DataId[idt])!= 13)) continue;
 	    }
 	    ///Compute preliminary particles distance
 	    Double_t dPt  = (DataPt[idt]-McPt[imc])/(scale_dPt);
